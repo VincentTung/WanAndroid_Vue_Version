c70b3f3e330698a1615e52a4599068af976218e1
dongxx <dongxx>
2019-08-09 11:23:22 +0800

    配置 stylus

--- WanAndroid_Vue/vuecode/vue_wanandroid/src/components/tab/tab.vue
+++ WanAndroid_Vue/vuecode/vue_wanandroid/src/components/tab/tab.vue
@@ -1,48 +1,52 @@
 <template>
   <div class="tab">
-
-
-      <router-link tag="div" class="tab-item" to="/mainpage">
+    <router-link tag="div" class="tab-item" to="/mainpage">
       <span class="tab-link">首页</span>
-      </router-link>
-    
-     <router-link tag="div" class="tab-item" to="/knowledge">
+    </router-link>
+
+    <router-link tag="div" class="tab-item" to="/knowledge">
       <span class="tab-link">知识体系</span>
-      </router-link>
-    
+    </router-link>
 
-     <router-link tag="div" class="tab-item" to="/wxaccount">
+    <router-link tag="div" class="tab-item" to="/wxaccount">
       <span class="tab-link">公众号</span>
-      </router-link>
-    
+    </router-link>
 
-     <router-link tag="div" class="tab-item" to="/project">
+    <router-link tag="div" class="tab-item" to="/project">
       <span class="tab-link">项目</span>
-      </router-link>
-    
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-    name:'Tab'
-}
+  name: "Tab"
+};
 </script>
 
-<style>
-
-.tab{
+<style  lang="stylus">
+.tab {
   display: flex;
   height: 44px;
   line-height: 44px;
+}
+
+.tab-item {
+  flex: 1;
+  text-align: center;
   
-}
-.tab-item{
-  flex:1;
-  text-align:center;
-}
-.tab-link{
-  padding-bottom: 5px;
+
+  .tab-link{
+
+   padding-bottom: 5px;
+    color :black;
+
+  }
+  &.router-link-active{
+    .tab-link{
+    color: blue;
+    }
+  }
   
 }
 </style>
