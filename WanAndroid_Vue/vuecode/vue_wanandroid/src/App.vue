c56de3da2593981bc6775485736347c95b1fc377
dongxx <dongxx>
2020-01-08 09:05:30 +0800

    Update App.vue

--- WanAndroid_Vue/vuecode/vue_wanandroid/src/App.vue
+++ WanAndroid_Vue/vuecode/vue_wanandroid/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
+    <div class="top"></div>
     <router-view></router-view>
     <Tab></Tab>
-  
   </div>
 </template>
 
@@ -11,19 +11,20 @@
 import Tab from '@/components/tab/tab'
 export default {
   name: 'App',
-  components:{
-      Tab
+  components: {
+    Tab
   }
 }
 </script>
 
 <style>
+.top {
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
